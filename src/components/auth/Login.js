6804477fe85c79fcf966d05000fd9f91dbc862e7
ChrisRM <chris.ricardo.r.m@gmail.com>
2021-01-21 16:29:22 -0600

    Add login state

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,12 +1,36 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 const Login = () => {
-    const onChange = () => {
+    // State para iniciar sesion
+    const [user, setUser] = useState({
+        email: '',
+        password: ''
+    });
 
+    // Extraer usuario
+    const { email, password } = user;
+
+    const onChange = e => {
+        setUser({
+            ...user,
+            [e.target.name] : e.target.value
+        })
     }
-    return ( 
+
+    //Login
+    const onSubmit = e => {
+        e.preventDefault()
+        // Validar que no haya campos vacios
+        // Pasarlo al action
+    }
+
+    return (
         <div className="form-usuario">
             <div className="contenedor-form sombra-dark">
                 <h1>Login</h1>
-                <form>
+                <form
+                    onSubmit={onSubmit}
+                    >
                     <div className="campo-form">
                         <label htmlFor="email">Email</label>
                         <input
@@ -14,6 +38,7 @@ const Login = () => {
                             id="email"
                             name="email"
                             placeholder="Your email"
+                            value={email}
                             onChange={onChange}
                         />
                     </div>
@@ -24,19 +49,21 @@ const Login = () => {
                             id="password"
                             name="password"
                             placeholder="Your password"
+                            value={password}
                             onChange={onChange}
                         />
                     </div>
                     <div className="campo-form">
-                        <input 
-                        type="submit" 
-                        className="btn btn-primario btn-block" 
-                        value="Login"/>
+                        <input
+                            type="submit"
+                            className="btn btn-primario btn-block"
+                            value="Login" />
                     </div>
                 </form>
+                <Link to={'/register'} className="enlace-cuenta">Crear una cuenta</Link>
             </div>
         </div>
-        );
+    );
 }
- 
+
 export default Login;
