34cb1ed263ad6d14bae32110e0fb0a370b9dcdb5
ChrisRM <chris.ricardo.r.m@gmail.com>
2021-01-25 16:42:23 -0600

    Add state to new project

--- src/components/projects/AddProject.js
+++ src/components/projects/AddProject.js
@@ -1,6 +1,34 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+
 
 const AddProject = () => {
+    // State para proyecto
+    const [project, setProject] = useState({
+        name: ''
+    });
+
+    // Extraer nombre de proyecto
+    const { name } = project;
+
+    // Lee contenidos del input
+    const onChangeProject = e => {
+        setProject({
+            ...project,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    // Cuando el usuario envia un proyecto
+    const onSubmitProyecto = e => {
+        e.preventDefault()
+
+        // Validar el proyecto
+
+        // Agregar al state
+
+        // Reiniciar el form
+    }
+
     return (
         <Fragment>
             <button
@@ -8,12 +36,17 @@ const AddProject = () => {
                 className=" btn btn-block btn-primario">
                 New Proyect
             </button>
-            <form className="formulario-nuevo-proyecto">
+            <form 
+            className="formulario-nuevo-proyecto"
+            onSubmit={onSubmitProyecto}
+            >
                 <input
                     type="text"
                     className="input-text"
                     placeholder="Project Name"
                     name="name"
+                    value={name}
+                    onChange={onChangeProject}
                 />
                 <input
                     type="submit"
