29ec0ff7b1afd5318959fc15d9d5834087293bed
ChrisRM <chris.ricardo.r.m@gmail.com>
2021-01-25 18:31:13 -0600

    Add layout header

---
+++ src/components/layout/Bar.js
@@ -0,0 +1,14 @@
+const Bar = () => {
+    return ( 
+        <header className="app-header">
+            <p className="nombre-usuario">
+                Hi <span>Chris</span>
+            </p>
+            <nav className="nav-principal">
+                <a href="#!">Log out</a>
+            </nav>
+        </header>
+     );
+}
+ 
+export default Bar;
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,9 +1,11 @@
 import Sidebar from "../layout/Sidebar";
+import Bar from "../layout/Bar";
 const Projects = () => {
     return (
         <div className="contenedor-app">
             <Sidebar />
             <div className="seccion-principal">
+                <Bar />
                 <main>
                     <div className="contenedor-tareas">
 
