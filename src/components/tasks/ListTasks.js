a02ede1bc2b7fbb57c1dd8d6629229063bdd7e4e
ChrisRM <chris.ricardo.r.m@gmail.com>
2021-01-27 16:54:35 -0600

    Design task list

--- src/components/tasks/ListTasks.js
+++ src/components/tasks/ListTasks.js
@@ -14,11 +14,16 @@ const ListTasks = () => {
                 Project: Tienda virtual
             </h2>
             <ul className="listado-tareas">
-                {tasksProject.length === 0 
-                ? (<li className="tarea"><p>No hay tareas</p></li>) 
-                : tasksProject.map(task => (<Task task={task} />))
+                {tasksProject.length === 0
+                    ? (<li className="tarea"><p>No hay tareas</p></li>)
+                    : tasksProject.map(task => (<Task task={task} />))
                 }
             </ul>
+            <button
+                type="button"
+                className="btn btn-eliminar">
+                Delete project &times;
+            </button>
         </Fragment>
 
     );
