4db2dcc58558ef344c584abd68c87a58e6a2ed2f
ChrisRM <chris.ricardo.r.m@gmail.com>
2021-01-29 15:29:03 -0600

    Add conditional rendering form task

--- src/components/tasks/FormTask.js
+++ src/components/tasks/FormTask.js
@@ -1,4 +1,17 @@
+import { useContext } from "react";
+import projectContext from "../../context/projects/projectContext";
+
 const FormTask = () => {
+    // Extraer si un proyecto esta activo
+    const projectsContext = useContext(projectContext)
+    const { selectedProject } = projectsContext;
+
+    // Si no hay proyecto seleccionado
+    if(!selectedProject) return null
+
+    // Array destructuring para extraer el proyecto actual
+    const [actualProject] = selectedProject;
+
     return (
         <div className="formulario">
             <form>
