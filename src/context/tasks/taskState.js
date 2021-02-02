1255a671a12ff4234e09c025aa4ff9e9613e52ac
ChrisRM <chris.ricardo.r.m@gmail.com>
2021-02-02 12:23:45 -0600

    Add task state

---
+++ src/context/tasks/taskState.js
@@ -0,0 +1,21 @@
+import { useReducer } from "react";
+import TaskContext  from './taskContext';
+import TaskReducer from "./taskReducer";
+
+const TaskState = props => {
+    const initialState = {
+        taskt: []
+    }
+
+    // Crear dispatch y state
+    const [state, dispatch] = useReducer(TaskReducer, initialState)
+    
+    return (
+        <TaskContext.Provider>
+            {props.children}
+        </TaskContext.Provider>
+    )
+
+}
+
+export default TaskState;
