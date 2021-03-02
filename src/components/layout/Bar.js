71a6c40ab9b7077e99e6149cf2e6ae9de7b5c473
ChrisRM <chris.ricardo.r.m@gmail.com>
2021-03-02 14:08:49 -0600

    Add Display User name on bar

--- src/components/layout/Bar.js
+++ src/components/layout/Bar.js
@@ -1,14 +1,29 @@
+import { useContext, useEffect } from 'react'
+import AuthContext from '../../context/auth/authContext'
+
 const Bar = () => {
-    return ( 
+    // Extraer la información de autenticación
+    const authContext = useContext(AuthContext)
+    const { user, authenticatedUser } = authContext
+
+    useEffect(() => {
+        authenticatedUser()
+    })
+
+
+    return (
         <header className="app-header">
-            <p className="nombre-usuario">
-                Hi <span>Chris</span>
-            </p>
+            {user ?
+                <p className="nombre-usuario">
+                    Hi <span>{user.name}</span>
+                </p>
+                : null}
+
             <nav className="nav-principal">
                 <a href="#!">Log out</a>
             </nav>
         </header>
-     );
+    );
 }
- 
+
 export default Bar;
